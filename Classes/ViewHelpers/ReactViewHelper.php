32de4a719e6109a6172d7b4449f4acf121856556
Johannes Goslar <jogo@kronberger-spiele.de>
2019-03-20 03:10:23 -0700

    prepare ssr

--- Classes/ViewHelpers/ReactViewHelper.php
+++ Classes/ViewHelpers/ReactViewHelper.php
@@ -7,6 +7,8 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 class ReactViewHelper extends AbstractViewHelper
 {
+    protected $escapeOutput = false;
+
     public static function renderStatic(
         array $arguments,
         \Closure $renderChildrenClosure,
