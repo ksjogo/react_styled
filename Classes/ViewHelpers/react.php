30b0e6398e74258e6266dbc8fcde234bad1643a5
Johannes Goslar <jogo@kronberger-spiele.de>
2019-03-20 01:28:31 -0700

    prepare ssr

--- Classes/ViewHelpers/react.php
+++ Classes/ViewHelpers/react.php
@@ -1,1 +1,28 @@
 <?php
+function PHPReact(string $filename)
+{
+    $basepath = realpath('../../' . __DIR__);
+    $dirPath = realpath($basepath . './');
+    $indexPath = realpath($basepath . './index.js');
+    $scriptPath = realpath($basepath . './' . $filename);
+
+    $oldDir = getcwd();
+    chdir($dirPath);
+    $command = "node ${indexPath} ${scriptPath}";
+    //echo $command;
+
+    try {
+        exec($command, $output);
+    } catch (Exception $e) {
+        //echo $e;
+    }
+    chdir($oldDir);
+    return $output;
+}
+
+if (php_sapi_name() == "cli") {
+    $output = PHPReact('./SSR/example.tsx');
+    foreach ($output as $line) {
+        echo "$line\n";
+    }
+}
